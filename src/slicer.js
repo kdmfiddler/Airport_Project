f411e7cd5716c2c2a8f21a46c9e38981bb2eac6d
kdmfiddler <kylemoored90@gmail.com>
2026-01-04 07:16:29 -0600

    Comments in slicer

--- src/slicer.js
+++ src/slicer.js
@@ -1,8 +1,10 @@
+// The following functions ammend items from data sources to be compiled in the correct format for the dashboard
+// These come from the aviation weather METAR API with potential use for TAFs when forcast analysis is added
 const timestamp = coded => {
     const loc = coded.indexOf("T");
-    /*if(loc == -1 || (coded.length - loc) < 6){
+    if(loc === -1 || (coded.length - loc) < 6){
         return ('##:##')
-    }*/
+    }
     let start = loc + 1;
     let end = loc + 6;
     const decoded = coded.slice(start, end);
@@ -10,16 +12,19 @@ const timestamp = coded => {
 
 }
 
+//Farenheight temperature conversiont "s for switch temp
 const sTemp = value => {
     let f = Math.round((value * 1.8) + 32);
     return f;   
 }
 
+//Getting rid of the "K" for the sign; eventually will handle "P" for Alaska & Hawaii without need for edits
 const iatac = icao => {
     let iata = icao.slice(1,4);
     return iata;
 }
 
+//Sometimes wind gust is null, making it zero for purposes of the page. 0 is filtered out and not displayed with CSS logic.
 const retrieve_wgst = value => {
     if (value === undefined){
         return 0;
@@ -28,6 +33,8 @@ const retrieve_wgst = value => {
         return value;
     }
 }
+
+//The following function flattens the data from AirportDB.io so necessary data can be retrieved with dot notation.
 
 const flatdb = (data, prefix = '') => {
   const flat = {};
@@ -50,6 +57,9 @@ const flatdb = (data, prefix = '') => {
   return flat;
 };
 
+/*Removes unnecessary descriptors such as municipality and "airport" to display a concise title (i.e. Hartsfield-Jackson International
+vs Atlanta Hartsfield Jackson International Airport)*/
+
 const aptname = data => {
   let afldname = data.name;
   let municipality = data.municipality;
@@ -69,6 +79,9 @@ const aptname = data => {
     
 };
 
+/*Retrieves a list of available runway headings from AirportDB.io (not idents). This is later used to analyze whether there are likely crosswinds.
+For now considered >30 degree divergence from actual runway heading*/ 
+
 const rwyhs = data => {
     const hdngs = Object.entries(data)
         .filter(([key]) => key.includes('heading_degT'))
@@ -77,6 +90,7 @@ const rwyhs = data => {
     return uhdngs;
 };
 
+//Gamut calls all the METAR related items to return a dictionary of values properly formated for the dashboard
 const gamut = data => {
     console.log('GAMUT CALLED WITH:', data.icaoId);
     let dict = {};
@@ -93,6 +107,7 @@ const gamut = data => {
     return dict;
 }
 
+//Gauntlet does what gamut does, but for AirportDB.io data
 const gauntlet = data => {
     const flatData = flatdb(data);
     let dict = {};
@@ -102,6 +117,8 @@ const gauntlet = data => {
     return dict;
 }
 
+//This function utilizes the wind direction collected from the METAR/TAF and the runway heading collected from AirportDB to assigned true or false to the 
+//crosswind variable
 const crossdet = (windir, hdngs) => {
     let cross = true;
     let indicate, adjust;
@@ -132,4 +149,5 @@ const crossdet = (windir, hdngs) => {
     return cross;
 }
 
+//Export the functions called outside the app; others are used by these function as data constructors
 export { gamut, gauntlet, crossdet };
